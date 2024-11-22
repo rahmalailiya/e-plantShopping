02dbdea55ef4915444b90c7bc16200b17e33b29f
rahmalailiya <rahmalailiya4@gmail.com>
2024-11-22 09:20:10 -0500

    Completed: Product listing page with 'add to cart' feature

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -6,7 +6,8 @@ import { addItem } from './CartSlice';
 function ProductList() {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
-    const [addedToCart, setAddedToCart] = useState({}) 
+    const [addedToCart, setAddedToCart] = useState({})
+    const [totalItems, setTotalItems] = useState(0)
     const dispatch = useDispatch()
     const plantsArray = [
         {
@@ -256,6 +257,7 @@ function ProductList() {
             ...prevState,
             [product.name]: true
         }))
+        setTotalItems((prevTotal) => prevTotal + 1);
     }
     return (
         <div>
@@ -274,7 +276,11 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+                    <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                        <span className='cart_quantity_count'>
+                            {totalItems}
+                        </span>
+                    </h1></a></div>
                 </div>
             </div>
             {!showCart ? (
@@ -292,7 +298,7 @@ function ProductList() {
                                             <img className='product-image' src={plant.image} alt={`Image of ${plant.name}`} />
                                             <h3 className="product-price">{plant.cost}</h3>
                                             <i><p>{plant.description}</p></i>
-                                            <button className='product-button' onClick={() => handleAddToCart(plant)}>Add To Cart</button>
+                                            <button className={addedToCart[plant.name] ? "product-button product-button-disabled" : "product-button"} onClick={() => handleAddToCart(plant)} disabled={addedToCart[plant.name]}>{addedToCart[plant.name] ? 'Added To Cart' : 'Add To Chart'}</button>
                                         </div>
                                     ))}
                                 </div>
